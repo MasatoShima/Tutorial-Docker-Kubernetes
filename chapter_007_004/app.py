0bdf2c20f6bd329da27b2ab116d873bf91a9c4a3
MasatoShima <masato.shima@icloud.com>
2020-01-01 23:10:55 +0900

    Fix

--- chapter_007_004/app.py
+++ chapter_007_004/app.py
@@ -39,11 +39,15 @@ def hello_world(req, res):
 # **************************************************
 # ----- Method health
 # **************************************************
+# Liveness Probe 用の handler
 @api.route("/health")
 def health(req, res):
 	elapsed_time = datetime.datetime.today() - START_TIME
 
-	if elapsed_time.seconds <= 20:
+	# 擬似的に起動から 20秒間を起動処理中とする
+	# 起動から 20秒以内は 500応答を返す
+	# 起動から 20秒以降は 200応答を返す
+	if elapsed_time.seconds <= 40:
 		res.status_code = api.status_codes.HTTP_200
 	else:
 		res.status_code = api.status_codes.HTTP_500
@@ -54,10 +58,13 @@ def health(req, res):
 # **************************************************
 # ----- Method ready
 # **************************************************
+# Rediness Probe 用の handler
 @api.route("/ready")
 def ready(req, res):
 	elapsed_time = datetime.datetime.today() - START_TIME
 
+	# 起動から 20秒以内は 500応答を返す
+	# 起動から 20秒以降は 200応答を返す
 	if elapsed_time.seconds <= 20:
 		res.status_code = api.status_codes.HTTP_500
 	else:
