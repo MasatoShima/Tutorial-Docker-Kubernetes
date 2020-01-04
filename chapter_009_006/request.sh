5d173a0c97e98b7572af25bd847f114135e02049
MasatoShima <masato.shima@icloud.com>
2020-01-05 01:15:19 +0900

    Fix

--- chapter_009_006/request.sh
+++ chapter_009_006/request.sh
@@ -5,8 +5,8 @@
 # Created on: 2020/01/05
 # """
 
-for pod in $(kubectl get pods | awk 'NR>1 {print $1}' | grep web-deploy); \
-  do kubectl exec "$pod" -- /bin/sh -c "hostname > /usr/share/nginx/html/index.html"; \
+for pod in $(kubectl get pods | awk 'NR>1 {print $1}' | grep web-deploy);
+  do kubectl exec "$pod" -- /bin/sh -c "hostname > /usr/share/nginx/html/index.html";
 done
 
 # End
